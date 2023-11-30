4fbb9ab3ccc3cde96d853e7e91eeaa61f7d66ccf
3x3cut0r <executor55@gmx.de>
2023-11-30 16:16:51 +0100

    added set_temp_sensor_pin

--- RaspberryPi/Pico/Heizungssteuerung/microcontroller/src/temp.py
+++ RaspberryPi/Pico/Heizungssteuerung/microcontroller/src/temp.py
@@ -4,15 +4,25 @@ from onewire import OneWire  # OneWire
 from ds18x20 import DS18X20  # DS180B20
 
 # load config
-from src.config import load_config, save_config
+from src.config import get_value, set_value
 
 # setup temperature sensor
-ds_pin = Pin(int(config["TEMP_SENSOR_PIN"]))
-ds_sensor = DS18X20(OneWire(ds_pin))
+temp_sensor_pin = Pin(int(get_value("TEMP_SENSOR_PIN")))
+temp_sensor = DS18X20(OneWire(temp_sensor_pin))
 
 # ==================================================
 # functions
 # ==================================================
+
+
+# set temp sensor pin
+def set_temp_sensor_pin(pin):
+    global temp_sensor_pin
+    try:
+        set_value("TEMP_SENSOR_PIN", int(pin))
+        temp_sensor_pin = Pin(int(pin))
+    except OSError as e:
+        print("error set temp sensor pin: ", e)
 
 
 # set temp sensor resolution
@@ -22,33 +32,25 @@ ds_sensor = DS18X20(OneWire(ds_pin))
 #   11     0,125 °C   375,00 ms
 #   12   0,00626 °C   750,00 ms
 def set_temp_resolution(bits=9):
-    # load config
-    config = load_config()
-
     # set temp resolution
     if int(bits) >= 9 and int(bits) <= 12:
         resolution = int(bits)
     else:
         resolution = 9
 
-    config["TEMP_SENSOR_RESOLUTION_BIT"] = resolution
-    save_config(config)
+    set_value("TEMP_SENSOR_RESOLUTION_BIT", resolution)
 
 
 # read temperature
 def read_temp():
-    # load config
-    config = load_config()
-
     # read temps
-    roms = ds_sensor.scan()
-    ds_sensor.convert_temp()
+    roms = temp_sensor.scan()
+    temp_sensor.convert_temp()
     for rom in roms:
         temp = float(
-            ds_sensor.read_temp(rom, int(config["TEMP_SENSOR_RESOLUTION_BIT"]))
+            temp_sensor.read_temp(rom, int(get_value("TEMP_SENSOR_RESOLUTION_BIT")))
         )
-        config["current_temp"] = temp
-        save_config(config)
+        set_value("current_temp", temp)
 
         # return only first temp found
         return temp
