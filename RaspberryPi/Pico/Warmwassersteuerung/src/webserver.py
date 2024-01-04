96c65f090371f2233af258632fa67f831fb77891
3x3cut0r <executor55@gmx.de>
2024-01-04 18:59:42 +0100

    replace_placeholder() added

--- RaspberryPi/Pico/Warmwassersteuerung/src/webserver.py
+++ RaspberryPi/Pico/Warmwassersteuerung/src/webserver.py
@@ -1,8 +1,10 @@
 # imports
 import re
+from io import StringIO
 import uasyncio as asyncio  # https://docs.micropython.org/en/latest/library/asyncio.html
 
-# from src.config import get_value
+# load config
+from src.config import load_config
 from src.lcd import get_lcd_list
 
 # setup file_name
@@ -30,9 +32,60 @@ def load_file(file_name, mode="r"):
 # get lcd lines
 def get_lcd_lines():
     lcd_lines = get_lcd_list()
-    return "".join(
-        ["<div class='lcd-line'>" + convert_html(line) + "</div>" for line in lcd_lines]
-    )
+    buffer = StringIO()
+    for line in lcd_lines:
+        buffer.write("<div class='lcd-line'>" + convert_html(line) + "</div>")
+    return buffer.getvalue()
+
+
+# replace placeholder
+def replace_placeholder(content=""):
+    config = load_config()
+
+    placeholders = {
+        "wifi_ssid": str,
+        "wifi_country": str,
+        "wifi_max_attempts": int,
+        "delay_before_start_1": int,
+        "delay_before_start_2": int,
+        "init_relay_time": int,
+        "update_time": int,
+        "relay_time": int,
+        "nominal_min_temp": float,
+        "nominal_max_temp": float,
+        "temp_update_interval": int,
+        "lcd_i2c_backlight": int,
+        "sensor_resolution_bit": int,
+        "previous_millis": int,
+        "interval": int,
+        "current_temp": float,
+        "temp_last_measurement": float,
+        "temp_last_measurement_time": int,
+        "temp_sampling_interval": int,
+        "temp_change_category": str,
+        "temp_change_high_threshold": float,
+        "temp_change_medium_threshold": float,
+        "TEMP_SENSOR_PIN": int,
+        "TEMP_SENSOR_RESOLUTION_BIT": int,
+        "LCD_PIN_SDA": int,
+        "LCD_PIN_SCL": int,
+        "LCD_ADDR": str,
+        "LCD_FREQ": int,
+        "LCD_COLS": int,
+        "LCD_ROWS": int,
+        "RELAY_OPEN_PIN": int,
+        "RELAY_CLOSE_PIN": int,
+        "BUTTON_TEMP_UP_PIN": int,
+        "BUTTON_TEMP_DOWN_PIN": int,
+        "LED": int,
+    }
+
+    for key, value_type in placeholders.items():
+        placeholder = f"<!--{key}-->"
+        value = config.get(key, "")
+        content = content.replace(placeholder, str(value))
+
+    return content
 
 
 # handle client
@@ -57,9 +110,8 @@ async def handle_client(reader, writer):
     # index.html
     if requested_path == "/" or requested_path == "index.html":
         response_content = load_file("index.html")
-        response_content = response_content.replace(
-            "<!--LCD_LINES_PLACEHOLDER-->", get_lcd_lines()
-        )
+        response_content = response_content.replace("<!--LCD_LINES-->", get_lcd_lines())
+        response_content = replace_placeholder(response_content)
 
     # styles.css
     elif requested_path == "/styles.css":
