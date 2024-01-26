7fa4e08976b22eeac172e298841f166d7e665ca7
3x3cut0r <executor55@gmx.de>
2024-01-26 10:37:52 +0100

    VERBOSE fix

--- RaspberryPi/Pico/Warmwassersteuerung/src/log.py
+++ RaspberryPi/Pico/Warmwassersteuerung/src/log.py
@@ -6,9 +6,15 @@ def log(level="INFO", message=""):
     level = level.upper()
     levels = {"OFF": 0, "ERROR": 1, "WARN": 2, "INFO": 3, "VERBOSE": 4}
     log_level = config.get_value("log_level", "OFF").upper()
+
+    # disable log, if log_level is not valid
     if log_level not in levels:
         log_level = "OFF"
-    if level == "VERBOSE":
-        level = "INFO"
+
     if log_level != "OFF" and levels[level] <= levels[log_level]:
+        # convert VERBOSE to INFO
+        if level == "VERBOSE":
+            level = "INFO"
+
+        # print log
         print(f"{level}: {message}")
