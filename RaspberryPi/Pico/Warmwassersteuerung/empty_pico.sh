d83010af5bdfce6d4624c4284ce9c43d16c485c2
3x3cut0r <executor55@gmx.de>
2024-03-11 08:36:43 +0100

    skip errors

--- RaspberryPi/Pico/Warmwassersteuerung/empty_pico.sh
+++ RaspberryPi/Pico/Warmwassersteuerung/empty_pico.sh
@@ -1,11 +1,12 @@
 #!/bin/zsh
 ampy --port /dev/cu.usbmodem11301 ls
 echo "deleting files ..."
-ampy --port /dev/cu.usbmodem11301 rm boot.py
-ampy --port /dev/cu.usbmodem11301 rm main.py
-ampy --port /dev/cu.usbmodem11301 rm config.json
-ampy --port /dev/cu.usbmodem11301 rm config_backup.json
-ampy --port /dev/cu.usbmodem11301 rmdir src
-ampy --port /dev/cu.usbmodem11301 rmdir web
+ampy --port /dev/cu.usbmodem11301 rm boot.py 2>/dev/null
+ampy --port /dev/cu.usbmodem11301 rm main.py 2>/dev/null
+ampy --port /dev/cu.usbmodem11301 rm config.json 2>/dev/null
+ampy --port /dev/cu.usbmodem11301 rm config_backup.json 2>/dev/null
+ampy --port /dev/cu.usbmodem11301 rm config_pico.json 2>/dev/null
+ampy --port /dev/cu.usbmodem11301 rmdir src 2>/dev/null
+ampy --port /dev/cu.usbmodem11301 rmdir web 2>/dev/null
 echo "deleting files ... DONE"
 ampy --port /dev/cu.usbmodem11301 ls
