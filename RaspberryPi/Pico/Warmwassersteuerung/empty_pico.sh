df4061c49e73e2aa5d2dc7c72742630bfa706e17
3x3cut0r <executor55@gmx.de>
2024-03-11 23:29:57 +0100

    config_pico removed

--- RaspberryPi/Pico/Warmwassersteuerung/empty_pico.sh
+++ RaspberryPi/Pico/Warmwassersteuerung/empty_pico.sh
@@ -5,7 +5,6 @@ ampy --port /dev/cu.usbmodem11301 rm boot.py 2>/dev/null
 ampy --port /dev/cu.usbmodem11301 rm main.py 2>/dev/null
 ampy --port /dev/cu.usbmodem11301 rm config.json 2>/dev/null
 ampy --port /dev/cu.usbmodem11301 rm config_backup.json 2>/dev/null
-ampy --port /dev/cu.usbmodem11301 rm config_pico.json 2>/dev/null
 ampy --port /dev/cu.usbmodem11301 rmdir src 2>/dev/null
 ampy --port /dev/cu.usbmodem11301 rmdir web 2>/dev/null
 echo "deleting files ... DONE"
