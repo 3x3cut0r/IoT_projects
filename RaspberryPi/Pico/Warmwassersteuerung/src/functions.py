2bd8564f6808ab4a7e3cf185d7b4bc95be5ed3f8
3x3cut0r <julianreith@gmx.de>
2024-11-03 10:07:38 +0100

    stop_timer moved

--- RaspberryPi/Pico/Warmwassersteuerung/src/functions.py
+++ RaspberryPi/Pico/Warmwassersteuerung/src/functions.py
@@ -171,9 +171,6 @@ async def set_relay(pin, relay_time):
     relay_open_pin = config.get_int_value("RELAY_OPEN_PIN", 14)
     relay_close_pin = config.get_int_value("RELAY_CLOSE_PIN", 15)
 
-    # set stop timer
-    config.set_value("stop_timer", relay_time // 1000 + 1)
-
     # only switch if the temperature can be read
     if 0 < current_temp <= 120:
         if pin == relay_open_pin:
@@ -195,6 +192,9 @@ async def open_relays(relay_time=config.get_int_value("relay_time", 1200)):
     nominal_max_temp = config.get_float_value("nominal_max_temp", 58.0)
     relay_open_pin = config.get_int_value("RELAY_OPEN_PIN", 14)
     relay_close_pin = config.get_int_value("RELAY_CLOSE_PIN", 15)
+
+    # set stop timer
+    config.set_value("stop_timer", relay_time // 1000 + 1)
 
     if current_temp < nominal_min_temp:
         # increase temp
